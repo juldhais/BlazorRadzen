cfb8eae04bb90a482303c1c0e801b0f27a3ccb40
juldhais <juldhais@gmail.com>
2024-10-27 16:57:59 +0700

    Add project files.

---
+++ Models/PersonModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorRadzen.Models;
+
+public class PersonModel
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateOnly? BirthDate { get; set; }
+    public string Gender { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+using BlazorRadzen;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddRadzenComponents();
+
+await builder.Build().RunAsync();
